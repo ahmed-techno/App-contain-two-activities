ff36a5271515a382d67ab55f1cd2a4e94fd0bee7
Ahmed AbdulKarim <ahmedtechno93@gmail.com>
2021-05-21 05:08:48 +0300

    use OOP in base activity by using Vector Type VB like TT

--- app/src/main/java/com/ahmedtechno93/app_contain_two_activities/ui/BaseActivity.kt
+++ app/src/main/java/com/ahmedtechno93/app_contain_two_activities/ui/BaseActivity.kt
@@ -1,4 +1,27 @@
 package com.ahmedtechno93.app_contain_two_activities.ui
 
-class BaseActivity {
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
+    abstract val LOG_TAG: String
+    abstract val bindingInflater:(LayoutInflater) -> VB
+    private var _binding : ViewBinding? = null
+
+    protected val binding = _binding as VB
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding=bindingInflater(layoutInflater)
+        setContentView(requireNotNull(_binding).root)
+        setup()
+        addCallbacks()
+    }
+    abstract fun setup()
+    abstract fun addCallbacks()
+    protected fun log(value: Any){
+        Log.v(LOG_TAG,value.toString())
+    }
 }
