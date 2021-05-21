f01c5a2e15a8e6bbcb1ccd7c676957ed3ce341ce
Ahmed AbdulKarim <ahmedtechno93@gmail.com>
2021-05-21 04:45:13 +0300

    finish Layout desgin

---
+++ app/src/main/java/com/ahmedtechno93/app_contain_two_activities/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.ahmedtechno93.app_contain_two_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
